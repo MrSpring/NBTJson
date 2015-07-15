4affa864465df5a6b16057fb19aefcc5a48b38b8
MrSpring <mrspring@mrspring.dk>
2015-07-15 22:44:44 +0200

    Initial Commit

---
+++ src/dk/mrspring/javanbt/NBTJson.java
@@ -0,0 +1,11 @@
+package dk.mrspring.javanbt;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * Created by Konrad on 15-07-2015.
+ */
+public class NBTJson
+{
+    public static NBTTagCompound
+}
